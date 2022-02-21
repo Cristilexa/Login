ba70436110250b1907375a4f1720c1a1ba7e9c37
Cristilexa <100146152+Cristilexa@users.noreply.github.com>
2022-02-21 16:15:39 -0300

    Update and rename login.js.js to login.js

---
+++ login.js
@@ -0,0 +1,20 @@
+var tentativa = 3;
+
+function validar(){
+	var email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	if (email == "admin" && password == "admin123"){
+	alert ("Boas fotos!");
+	return false; 
+} 
+	else{
+	tentativa --;
+	alert ("Tem mais" + tentativa + "tentativas");
+	if (tentativa == 0){
+	document.getElementById("email").disabled = true;
+	document.getElementById("password").disabled = true;
+	document.getElementById("enviar").disabled = true;
+	return false;
+}
+}
+}
